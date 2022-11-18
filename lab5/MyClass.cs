df362ae50d7e25918651c69d6977d0eca1585d65
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-11-18 14:16:47 +0200

    l
    l

---
+++ lab5/MyClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+{
+    internal class MyClass
+    {
+    public static T Factory<T>() where T : new() => new();
+    }
+}
---
+++ lab5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+{
+    internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.ReadKey(true);
+    }
+}
+}
